3ba9f860c97c57e331d8d278416d7603bf333faa
reiern70 <reiern70@gmail.com>
2013-02-27 11:18:13 +0100

    improve details

--- wicket-angular/src/test/java/com/antilia/angular/PersonDetailPanel.java
+++ wicket-angular/src/test/java/com/antilia/angular/PersonDetailPanel.java
@@ -3,6 +3,8 @@
  */
 package com.antilia.angular;
 
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
@@ -23,5 +25,7 @@ public class PersonDetailPanel extends Panel {
 	public PersonDetailPanel(String id, IModel<Person> model) {
 		super(id, model);
 		add(new Label("name", Model.of(model.getObject().getName() + " " +model.getObject().getLastName())));
+		
+		add(new WebMarkupContainer("image").add(new AttributeModifier("src", model.getObject().getBigImageUrl())));
 	}
 }
